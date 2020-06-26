82b2be26d209a19c5010236f4daee6f1cfaa0fb6
Aleezah1429 <64039135+Aleezah1429@users.noreply.github.com>
2020-06-27 02:09:16 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,39 @@
+function getNumber(num){
+    console.log(num)
+    var result = document.getElementById('result');
+
+    result.value += num
+
+}
+
+function clearResult(){
+    var result = document.getElementById('result');
+    result.value = ''
+}
+
+function getResult(){
+    var result = document.getElementById('result');
+    result.value = eval(result.value)
+}
+
+function backspace(){
+    var result = document.getElementById('result');
+    var x = result.value;
+    if(x.length>0){
+        x=x.substring(0,x.length-1);
+        result.value = x
+    }
+}
+
+var i = 0;
+function getbracket(){
+    var result = document.getElementById('result');
+    if(i==0){
+        result.value  += '(';
+        i=1;
+    }
+    else if(i==1){
+        result.value  += ')';
+        i=0;
+    }
+}
